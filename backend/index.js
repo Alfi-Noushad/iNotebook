c84185630da0f1b91ad977d65af02b301874154f
Alfi Noushad <Alfinoushad@gmail.com>
2025-06-16 19:23:37 +0530

    setup the stoarage

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,14 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello Alfi')
 })
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
