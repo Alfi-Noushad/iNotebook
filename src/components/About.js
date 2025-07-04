a89d726b8e180bf4ddf00e721bfae7db1c8b129f
Alfi Noushad <Alfinoushad@gmail.com>
2025-07-04 22:47:09 +0530

    setup about

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,38 @@
 import React from 'react'
 function About() {
-    return(
-    <div className='container'>
-     This is About 
-    </div>
+    return (
+        <div className='container'>
+            <div className="card">
+                <div className="card-header">
+                    INOTEBOOK
+                </div>
+                <div className="card-body">
+                    <figure>
+                        <blockquote className="blockquote d-flex">
+                            <div>iNotebook is a modern, secure, cloud-based notebook designed to help you store, organize, and access your books, notes, and ideas anytime, anywhere.
+                                With iNotebook, you no longer have to worry about losing your notes or carrying heavy notebooks. All your content is safely stored in the cloud, instantly synced across your devices, and ready whenever you need it.
+
+                                Whether it’s study material, personal journals, creative writing, or important references — iNotebook keeps everything organized and easily searchable, so you can focus on what matters most: your ideas.
+
+                                Features:
+                                <div className="point d-flex flex-column">
+                                    <div>✅ Create, edit, and manage your books and notes effortlessly</div>
+                                   <div> ✅ Access your notebook from any device, anytime</div>
+                                   <div> ✅ Secure cloud storage keeps your data safe</div>
+                                    <div>✅ Simple and clean interface for distraction-free writing.</div>
+                                </div>
+                         </div>
+                    
+                        </blockquote>
+                        <figcaption className="blockquote-footer">
+                             <cite title="Source Title my-3">Built for everyone</cite>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
     )
-  
+
 }
 
 export default About;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
+
 function Navbar() {
-let navigate = useNavigate();
+    let navigate = useNavigate();
+    
 
     const handleLogout = () => {
         localStorage.removeItem('token');
@@ -22,17 +24,23 @@ let navigate = useNavigate();
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname==="/"?"active":""}`} aria-current="page" to="/">Home</Link>
+                            <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname==="/about"?"active":""}`} to="/about">About</Link>
+                            <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
                         </li>
 
                     </ul>
-                    {!localStorage.getItem('token')?<form className="d-flex">
+                    {!localStorage.getItem('token') ? <form className="d-flex">
                         <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
                         <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
-                    </form>: <button onClick={handleLogout} className='btn btn-primary'>Logout</button>}
+                    </form> : <>  
+                        <div
+                            className="bg-primary text-white rounded-circle d-flex justify-content-center align-items-center mx-4"
+                            style={{ width: '40px', height: '40px', fontWeight: 'bold' }}>
+                            
+                        </div>
+                        <button onClick={handleLogout} className='btn btn-primary'>Logout</button> </>}
                 </div>
             </div>
         </nav>
